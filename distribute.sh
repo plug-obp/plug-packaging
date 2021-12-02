ef0632905455cec549ffa0ac8a30bca3238728df
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2021-12-02 12:29:43 +0100

    added a distribution script

---
+++ distribute.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGET=~/Downloads/obp2-distribution
+
+rsync -a obp2/build/distributions/ 			${TARGET}
+rsync -a obp2-headless/build/distributions/ 		${TARGET}
+rsync -a obp2-jpro/build/distributions/ 		${TARGET}
+rsync -a obp2-remote/build/distributions/ 		${TARGET}
+rsync -a obp2-remote-headless/build/distributions/ 	${TARGET}
+rsync -a obp2-simulator/build/distributions/ 		${TARGET} 
